dad682271df6fee536e34901c1ec895e7fced64e
Harshit Singh <54441781+Harshit-Singh-Shrinet@users.noreply.github.com>
2022-06-16 03:33:29 +0530

    this is my first commit.

---
+++ index.js
@@ -0,0 +1,19 @@
+//player 1
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumberImage1="images/dice"+randomNumber1+".png";
+document.querySelector("div .img1").setAttribute("src",randomNumberImage1);
+
+//player 2
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomNumberImage2="images/dice"+randomNumber2+".png";
+document.querySelector("div .img2").setAttribute("src",randomNumberImage2);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="Player 1 Wins The Game";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").textContent="Player 2 Wins The Game";
+}
+else{
+  document.querySelector("h1").textContent="Game is Draw";
+}
